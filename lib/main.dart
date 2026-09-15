8edd79939e88c2011363ff217435fb24db73034b
didhan <kasdhan43@gmail.com>
2026-09-15 10:15:44 +0700

    Membuat Form Interaktif Pertemuan 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 // import 'column_widget.dart';
 // import 'row_widget.dart';
-import 'first_widget.dart';
+// import 'first_widget.dart';
+import 'form_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         ), // ColorScheme.fromSeed
         useMaterial3: true,
       ), // ThemeData
-      home: const FirstWidget(),
+      home: const FormWidget(),
     ); // MaterialApp
   }
 }
